e5148165718213e58c9c9665a4753158410406c9
Derrick <derrick0105.cg@gmail.com>
2019-01-03 22:20:57 +0800

    django初始設定

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Derrick\.PyCharm2018.3\Software_Engineering_project\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
